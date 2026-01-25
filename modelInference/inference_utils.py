2bf512532fdd73c0bde8fcf4fbe978bc20a88d9e
Luchian <luchian-1999@mail.ru>
2026-01-25 03:09:20 +0300

    Implemented model generation pipeline

---
+++ modelInference/inference_utils.py
@@ -0,0 +1,88 @@
+import sys
+sys.path.append(r"c:\main\GitHub\handwrittenTextRecognitionSystem")
+
+from typing import Callable
+
+import PIL
+
+from utils import ViTSplit
+
+import numpy as np
+
+from transformers import PreTrainedTokenizerFast
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+
+
+
+class ModelInference:
+    def __init__(self, model: nn.Module, transformations: transforms.transforms.Compose,
+                 tokenizer: PreTrainedTokenizerFast, split: ViTSplit,
+                 soft_func: Callable[[torch.tensor], torch.tensor] = nn.Softmax(dim=-1),
+                 device: str = 'cpu'):
+        self.model = model
+        self.split = split
+        self.device = device
+        self.soft_func = soft_func
+        self.tokenizer = tokenizer
+        self.transform = transformations
+    
+    @property
+    def sos(self):
+        return self.tokenizer.get_vocab()["<sos>"]
+    
+    @property
+    def eos(self):
+        return self.tokenizer.get_vocab()["<eos>"]
+    
+    @property
+    def encoder(self):
+        return self.model.encoder
+    
+    @property
+    def decoder(self):
+        return self.model.decoder
+    
+    @property
+    def linear(self):
+        return self.model.linear
+
+    def generate(self, picture: PIL.JpegImagePlugin.JpegImageFile):
+        """
+        Given a single picture transforms it for ViT input and
+        generates text based on it
+        """
+        assert picture.mode == "L", "Picture should be in gray mode"
+        # Move model to evalutaion mode for inference
+        self.model.eval()
+        # Picture preparation for model inputing
+        tensor_img = self.transform(picture)
+        splitted_image = torch.cat(self.split(tensor_img), dim=0).unsqueeze(dim=0)
+        # Flatten patches to get encoder representations
+        flattened_patches = splitted_image.flatten(start_dim=-2, end_dim=-1)
+        encoder_features = self.encoder(flattened_patches)
+        # Generate index by index
+        gen_indecies = [self.sos]
+        while gen_indecies[-1] != self.eos:
+            N_seq = len(gen_indecies)
+            tensor_indecies = torch.tensor(gen_indecies, dtype=torch.long).unsqueeze(dim=0)
+            # Each time depending on sequence length the decoder mask dimension has to be changed accordingly
+            new_mask = torch.triu(torch.ones(N_seq, N_seq).to(device=self.device)).T
+            for decoder_layer in self.decoder.layers:
+                decoder_layer.mask = new_mask 
+            # Get decoder features to predict next token
+            decoder_features = self.decoder(tensor_indecies, x_dec=encoder_features).squeeze(dim=0)
+            vocab_pre_distribution = self.linear(decoder_features)
+            distribution = self.soft_func(vocab_pre_distribution)
+            # Add generated from distribution token and continue
+            generated_token = distribution[-1].argmax().item()
+            gen_indecies.append(generated_token)
+        return self.tokenizer.decode(gen_indecies, skip_special_tokens=True)
+
+'--------------------------------------------------------------------------------'
+    
+
+
