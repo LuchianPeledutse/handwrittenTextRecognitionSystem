62ae2d3dd4cf536b197f22a20d27f5b829885f84
Luchian <luchian-1999@mail.ru>
2026-01-26 23:26:44 +0300

    Finished model inference object

--- modelInference/inference_utils.py
+++ modelInference/inference_utils.py
@@ -30,6 +30,7 @@ class ModelInference:
         self.tokenizer = tokenizer
         self.transform = transformations
         self.max_seq_length = max_seq_length
+        self.initial_mask = model.decoder.mask
     
     @property
     def sos(self):
@@ -50,6 +51,10 @@ class ModelInference:
     @property
     def linear(self):
         return self.model.linear
+    
+    def update_decoder_mask(self, mask: torch.tensor):
+        for decoder_layer in self.decoder.layers:
+            decoder_layer.mask = mask
 
     def generate(self, picture: PIL.JpegImagePlugin.JpegImageFile):
         """
@@ -63,17 +68,16 @@ class ModelInference:
         tensor_img = self.transform(picture)
         splitted_image = torch.cat(self.split(tensor_img), dim=0).unsqueeze(dim=0)
         # Flatten patches to get encoder representations
-        flattened_patches = splitted_image.flatten(start_dim=-2, end_dim=-1)
+        flattened_patches = splitted_image.flatten(start_dim=-2, end_dim=-1).to(device=self.device)
         encoder_features = self.encoder(flattened_patches)
         # Generate index by index
         gen_indecies = [self.sos]
         while gen_indecies[-1] != self.eos and len(gen_indecies) < self.max_seq_length:
             N_seq = len(gen_indecies)
-            tensor_indecies = torch.tensor(gen_indecies, dtype=torch.long).unsqueeze(dim=0)
+            tensor_indecies = torch.tensor(gen_indecies, dtype=torch.long).unsqueeze(dim=0).to(device=self.device)
             # Each time depending on sequence length the decoder mask dimension has to be changed accordingly
             new_mask = torch.triu(torch.ones(N_seq, N_seq).to(device=self.device)).T
-            for decoder_layer in self.decoder.layers:
-                decoder_layer.mask = new_mask 
+            self.update_decoder_mask(new_mask)
             # Get decoder features to predict next token
             decoder_features = self.decoder(tensor_indecies, x_dec=encoder_features).squeeze(dim=0)
             vocab_pre_distribution = self.linear(decoder_features)
@@ -81,6 +85,8 @@ class ModelInference:
             # Add generated from distribution token and continue
             generated_token = distribution[-1].argmax().item()
             gen_indecies.append(generated_token)
+        # Return decoder to previous mask
+        self.update_decoder_mask(self.initial_mask)
         return self.tokenizer.decode(gen_indecies, skip_special_tokens=True)
 
 '--------------------------------------------------------------------------------'
