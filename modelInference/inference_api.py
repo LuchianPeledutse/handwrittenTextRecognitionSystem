ad5855489b5f1a531d1198af69559655886fbfb1
Luchian(Home) <luchianpeledutse@gmail.com>
2026-01-26 22:31:43 +0300

    Added io, base64 libraries for picture processing

--- modelInference/inference_api.py
+++ modelInference/inference_api.py
@@ -1,3 +1,6 @@
+import io
+import base64
+
 from PIL import Image
 
 import fastapi
@@ -14,6 +17,6 @@ class InputData(BaseModel):
 
 @app.post('/prediction')
 def predict(image_data: InputData):
-    image = Image.open(io.BytesIO(base64.b64decode(data.image_64_base))).convert("L")
-    return {"Image_size": image.size, "Image_mode": image.mode}
+    image = Image.open(io.BytesIO(base64.b64decode(image_data.image_64_base))).convert("L")
+    return {"image_size": image.size, "image_mode": image.mode}
 
